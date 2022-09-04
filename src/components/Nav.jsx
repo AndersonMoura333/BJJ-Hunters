f84d05e4e81b3f6ce1a4e06718e7f7c362c77134
AndersonMoura333 <andersonmouradasilva2@gmail.com>
2022-09-04 15:35:17 -0300

    first commit

---
+++ src/components/Nav.jsx
@@ -0,0 +1,17 @@
+import '../styles/Nav.css'
+export default function Nav() {
+    return(
+        <nav className="Nav">
+            <div className='nav-div'>
+
+            <a href="">inicio</a>
+            </div>
+
+            <div className='nav-div'>
+                <a href="">Campeonatos</a>
+                <a href="">Atletas</a>
+                <a href="">Quem somos</a>
+            </div>
+        </nav>
+        )
+}
---
+++ src/pages/App.jsx
@@ -0,0 +1,15 @@
+
+import Nav from '../components/Nav';
+import '../styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Nav></Nav>
+      </header>
+    </div>
+  );
+}
+
+export default App;
